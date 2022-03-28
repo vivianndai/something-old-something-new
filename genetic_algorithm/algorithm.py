8b1d6eb117ee1504e206888e12f4ed7697bc1b9a
Julie Helenek <julie.helenek@gmail.com>
2022-03-28 19:06:12 -0400

    image to array & array to image

--- genetic_algorithm/algorithm.py
+++ genetic_algorithm/algorithm.py
@@ -1,1 +1,18 @@
 # Runs the algorithm
+import numpy as np
+from PIL import Image
+
+def main():
+    print("Hello World!")
+    img = Image.open("static\images\starrynight.png")
+    np_img = np.array(img)
+    for i in range(90):
+        np_img[i] = [255,0,0]
+   
+
+    pilImage = Image.fromarray(np_img)
+    print(type(pilImage))
+    pilImage.show()
+    
+if __name__ == "__main__":
+    main()
