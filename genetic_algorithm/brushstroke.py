40c9636c4abca101a827c2137477a325926de58d
ct462 <101839938+ct462@users.noreply.github.com>
2022-05-09 03:54:31 -0400

    fixed sizing and made brushes transparent

--- genetic_algorithm/brushstroke.py
+++ genetic_algorithm/brushstroke.py
@@ -9,26 +9,36 @@ class Brushstroke():
     def __init__(self, target_dims):
         self.color = random.randrange(0, 255)
         self.brushNumber = 4
-        self.brushes = self.load_brushes('brush_types')
+        self.brushes = self.load_brushes(
+            '/Users/connietsang/Desktop/ai/brushstrokes/brush_types')
         self.brush_type = random.randrange(0, self.brushNumber)
 
         # representation of the brush img in matrix form
-        self.brush_rep = self.brushes[self.brush_type]
+        self.brush_matrix = self.brushes[self.brush_type]
 
-        self.posX = int(random.randrange(0, target_dims[0]))
-        self.posY = int(random.randrange(0, target_dims[1]))
-        # Figure out range of sizes of brushstrokes
-        self.size = 0
-        # Add rotation
-        # Figure out what padding number should be?
-        self.padding = 0
+        scale_percent = 30  # percent of original size
+        width = int(self.brush_matrix.shape[1] * scale_percent / 100)
+        height = int(self.brush_matrix.shape[0] * scale_percent / 100)
+        dim = (width, height)
+
+        self.brush_rep = cv2.resize(
+            self.brush_matrix, dim, interpolation=cv2.INTER_AREA)
+
+        # self.mask = self.brush_rep[:, :, 3]
+
+        self.size = (self.brush_rep.shape[0], self.brush_rep.shape[1])
+
+        self.posX = int(random.randrange(0, target_dims[0]) - width)
+        self.posY = int(random.randrange(0, target_dims[1]) - height)
+
         # Gradient measures change in the image
         # Magnitude tells us how quickly the image is changing
         # Direction of the gradient tells us the direction in which the image is changing most rapidly.
-        # TODO: Need to draw the whole image first to calculate gradient
 
     def load_brushes(self, path):
         brushes = []
         for i in range(self.brushNumber):
-            brushes.append(cv2.imread(path + '/brush' + str(i) + '.png'))
+            brushes.append(cv2.imread(
+                (path + '/brush' + str(i) + '.png'), cv2.IMREAD_UNCHANGED))
+            # brushes.append(cv2.imread(path + '/brush' + str(i) + '.png'))
         return brushes
